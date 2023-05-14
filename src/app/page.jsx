0595a945b9d04f7dbe2acecd1265477f56c70c12
Ran Pol <rnd809@gmail.com>
2023-05-14 00:17:08 -0400

    Adding the initial content of the landing page.

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,20 @@
 const Home = () => {
-  return <div>Home</div>
+  return (
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">
+        Descubre y Comparte
+        <br className="max-md:hidden" />
+        <span className="green_gradient text-center">
+          Consigas Impulsadas Por IA
+        </span>
+      </h1>
+      <p className="desc text-center">
+        PrompDescubrir es una herramienta de consignas de inteligencia
+        artificial de código abierto para el mundo moderno, que te permite
+        descubrir, crear y compartir consignas creativas.
+      </p>
+    </section>
+  )
 }
 
 export default Home
