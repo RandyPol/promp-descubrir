98b822e3c5674547e7a0733b2a65872424adc6fd
Ran Pol <rnd809@gmail.com>
2023-05-14 15:45:28 -0400

    Fix type for User import.

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,7 +1,7 @@
 import NextAuth from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
 
-import { User } from '@/models/user'
+import User from '@/models/user'
 import { connectToDatabase } from '@/utils/database'
 
 const handler = NextAuth({
@@ -11,31 +11,33 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
-  async session({ session }) {
-    const sessionUser = await User.findOne({ email: session.user.email })
+  callbacks: {
+    async session({ session }) {
+      const sessionUser = await User.findOne({ email: session.user.email })
 
-    session.user.id = sessionUser._id.toString()
+      session.user.id = sessionUser._id.toString()
 
-    return session
-  },
-  async signIn({ profile }) {
-    try {
-      await connectToDatabase()
-      const user = await User.findOne({ email: profile.email })
+      return session
+    },
+    async signIn({ profile }) {
+      try {
+        await connectToDatabase()
+        const user = await User.findOne({ email: profile.email })
 
-      if (!user) {
-        await User.create({
-          email: profile.email,
-          username: profile.name.replace(' ', '').toLowerCase(),
-          image: profile.picture,
-        })
+        if (!user) {
+          await User.create({
+            email: profile.email,
+            username: profile.name.replace(' ', '').toLowerCase(),
+            image: profile.picture,
+          })
+        }
+
+        return true
+      } catch (error) {
+        console.log(error)
+        return false
       }
-
-      return true
-    } catch (error) {
-      console.log(error)
-      return false
-    }
+    },
   },
 })
 
