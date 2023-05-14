ced5e33ebe0e88d9f63c5e193788567efe0a8592
Ran Pol <rnd809@gmail.com>
2023-05-14 00:56:55 -0400

    feat: Add feed component to home page.

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,3 +1,5 @@
+import Feed from '@/components/Feed'
+
 const Home = () => {
   return (
     <section className="w-full flex-center flex-col">
@@ -13,6 +15,7 @@ const Home = () => {
         artificial de código abierto para el mundo moderno, que te permite
         descubrir, crear y compartir consignas creativas.
       </p>
+      <Feed />
     </section>
   )
 }
