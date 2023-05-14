3b3577c5c2d808b3903a9bad2b981a48827150a6
Ran Pol <rnd809@gmail.com>
2023-05-14 10:45:41 -0400

    Add conditional rendering of nav items for logged in users

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -6,6 +6,7 @@ import { useState, useEffect } from 'react'
 import { singIn, signOut, useSession, getProviders } from 'next-auth/react'
 
 const Nav = () => {
+  const isUserLogged = true
   return (
     <nav
       className="flex-between w-full
@@ -14,13 +15,39 @@ const Nav = () => {
       <Link href="/" className="flex gap-2 flex-center">
         <Image
           src="/assets/images/logo.svg"
-          alt="Companys log"
+          alt="Company logo"
           width={30}
           height={30}
           className="object-contain"
         />
         <p className="logo_text">PrompDescubrir</p>
       </Link>
+
+      {/* Response design */}
+      <div className="sm:flex hidden">
+        {isUserLogged ? (
+          <div className="flex gap-3 md:gap-5">
+            <Link href="/create-prompt" className="black_btn">
+              Crear Publicación
+            </Link>
+            <button type="button" onClick={signOut} className="outline_btn">
+              Cerrar sesión
+            </button>
+            <Link href="/profile">
+              <Image
+                src="/assets/images/logo.svg"
+                alt="Profile"
+                width={35}
+                height={35}
+              />
+            </Link>
+          </div>
+        ) : (
+          <>
+            <Link href="/login" className="flex gap-2 flex-center" />
+          </>
+        )}
+      </div>
     </nav>
   )
 }
