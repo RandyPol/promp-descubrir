c4022b90eb146f5380f86ce620d725033f1c1eea
Ran Pol <rnd809@gmail.com>
2023-05-15 15:25:04 -0400

    feat: Add loading spinner for the edit prompt page

--- src/app/update-prompt/page.jsx
+++ src/app/update-prompt/page.jsx
@@ -2,8 +2,16 @@
 
 import { useState, useEffect } from 'react'
 import { useRouter, useSearchParams } from 'next/navigation'
+import { css } from '@emotion/react'
+import { ClipLoader } from 'react-spinners'
 
 import Form from '@components/Form'
+
+const override = css`
+  display: block;
+  margin: 0 auto;
+  border-color: red;
+`
 
 const EditPrompt = () => {
   const router = useRouter()
@@ -11,6 +19,7 @@ const EditPrompt = () => {
   const promptId = searchParams.get('id')
 
   const [submitting, setSubmitting] = useState(false)
+  const [loading, setLoading] = useState(true)
   const [post, setPost] = useState({
     prompt: '',
     tag: '',
@@ -49,18 +58,29 @@ const EditPrompt = () => {
         prompt: prompt.prompt,
         tag: prompt.tag,
       })
+      setLoading(false)
     }
     if (promptId) getPrompt()
   }, [promptId])
 
   return (
-    <Form
-      type="Editar"
-      post={post}
-      setPost={setPost}
-      submitting={submitting}
-      handleSubmit={handleSubmit}
-    />
+    <>
+      <ClipLoader
+        color={'#123abc'}
+        loading={loading}
+        css={override}
+        size={150}
+      />
+      {!loading && (
+        <Form
+          type="Editar"
+          post={post}
+          setPost={setPost}
+          submitting={submitting}
+          handleSubmit={handleSubmit}
+        />
+      )}
+    </>
   )
 }
 
