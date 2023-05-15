78355eafae55f5e562bfdc69c086ff49d54a769f
Ran Pol <rnd809@gmail.com>
2023-05-15 01:33:01 -0400

    Refactor PromptCard to include prompt data and user image in a card layout

--- src/components/PromptCard.jsx
+++ src/components/PromptCard.jsx
@@ -1,7 +1,26 @@
-import React from 'react'
+'use client'
 
-const PromptCard = () => {
-  return <div>PromptCard</div>
+import { useState } from 'react'
+import Image from 'next/image'
+import { useSession } from 'next-auth/react'
+import { usePathname, useRouter } from 'next/navigation'
+
+const PromptCard = ({ prompt, handleTagClick }) => {
+  return (
+    <div className="prompt_card">
+      <div className="flex justify-between items-start gap-5">
+        <div className="flex-1 flex justify-start items-center gap-3 cursor-pointer">
+          <Image
+            src={prompt.creator.image}
+            alt="user profile image"
+            width={35}
+            height={35}
+            className="rounded-full"
+          />
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default PromptCard
