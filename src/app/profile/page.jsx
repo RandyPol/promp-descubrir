0431c4ff5f345ba3527e7227eea0ab7e185f65ea
Ran Pol <rnd809@gmail.com>
2023-05-15 13:52:49 -0400

    Refactor: Add delete functionality and confirmation message

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -26,13 +26,29 @@ const ProfilePage = () => {
     router.push(`/update-prompt?id=${post._id}`)
   }
   const handleDelete = async (post) => {
-    console.log('delete')
+    const confirmDelete = confirm(
+      '¿Estás seguro de que quieres eliminar esta consigna?'
+    )
+
+    if (confirmDelete) {
+      try {
+        const res = await fetch(`/api/prompt/${post._id.toString()}`, {
+          method: 'DELETE',
+        })
+
+        const updatePosts = posts.filter((p) => p._id !== post._id)
+
+        setPosts(updatePosts)
+      } catch (error) {
+        console.log(error)
+      }
+    }
   }
 
   return (
     <>
       <Profile
-        name="John"
+        name="Account"
         desc="Perfil personalizado basado en las creaciones de consignas del usuario."
         data={posts}
         handleEdit={handleEdit}
