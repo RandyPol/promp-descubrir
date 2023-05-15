58826fe3c97257026b465dabc43d1c45e37c1033
Ran Pol <rnd809@gmail.com>
2023-05-15 00:14:40 -0400

    refactor: update the key and remove the extra prameter.

--- src/components/PromptCardList.jsx
+++ src/components/PromptCardList.jsx
@@ -3,9 +3,9 @@ import PromptCard from './PromptCard'
 const PromptCardList = ({ data, handleTagClick }) => {
   return (
     <div className="mt-16 prompt_layout">
-      {data.map((prompt, index) => (
+      {data.map((prompt) => (
         <PromptCard
-          key={index}
+          key={prompt._id}
           prompt={prompt}
           handleTagClick={handleTagClick}
         />
