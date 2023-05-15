68c5195f1ef4323903165d6b662522f16cab2ca0
Ran Pol <rnd809@gmail.com>
2023-05-15 01:50:06 -0400

    feat: Add the prompt name and creator info to the card as well as copy button.

--- src/components/PromptCard.jsx
+++ src/components/PromptCard.jsx
@@ -6,6 +6,7 @@ import { useSession } from 'next-auth/react'
 import { usePathname, useRouter } from 'next/navigation'
 
 const PromptCard = ({ prompt, handleTagClick }) => {
+  const [copied, setCopied] = useState(false)
   return (
     <div className="prompt_card">
       <div className="flex justify-between items-start gap-5">
@@ -16,6 +17,28 @@ const PromptCard = ({ prompt, handleTagClick }) => {
             width={35}
             height={35}
             className="rounded-full"
+          />
+
+          <div className="flex flex-col">
+            <h3 className="font-satoshi font-semibold">
+              {prompt.creator.username}
+            </h3>
+            <p className="font-inter text-sm text-gray-500">
+              {prompt.creator.email}
+            </p>
+          </div>
+        </div>
+
+        <div className="copy_btn" onClick={() => {}}>
+          <Image
+            src={
+              copied === prompt.prompt
+                ? '/assets/icons/tick.svg'
+                : '/assets/icons/copy.svg'
+            }
+            alt="clipboard icon"
+            width={15}
+            height={15}
           />
         </div>
       </div>
