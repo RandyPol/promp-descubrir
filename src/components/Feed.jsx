94e26f635be27e7895bf2d5cc6e88bb43d72ab64
Ran Pol <rnd809@gmail.com>
2023-05-14 23:57:01 -0400

    feat(Feed): add search functionality

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+'use client'
+
+import { useState, useEffect } from 'react'
+import PromptCard from './PromptCard'
 
 const Feed = () => {
+  const [searchText, setSearchText] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log('searchText', searchText)
+  }
+
   return (
-    <div>Feed</div>
+    <section className="feed">
+      <form onSubmit={handleSubmit} className="relative w-full flex-center">
+        <input
+          type="text"
+          placeholder="Buscar etiqueta o nombre de usuario"
+          value={searchText}
+          onChange={(e) => setSearchText(e.target.value)}
+          required
+          className="search_input peer"
+        />
+      </form>
+      <PromptCard />
+    </section>
   )
 }
 
