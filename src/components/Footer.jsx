78f9482b48c04b53434910ba393a8382a69a23b4
Ran Pol <rnd809@gmail.com>
2023-05-16 13:10:39 -0400

    Fix typo.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,12 +8,12 @@ const Footer = () => {
   const devGithub = 'https://github.com/RandyPol'
 
   return (
-    <footer class="bg-gray-200 text-gray-900 py-4 px-4">
-      <div class="flex mt-4 space-x-6 sm:justify-center sm:mt-0 items-center">
-        <p class="text-md font-satoshi">
+    <footer className="bg-gray-200 text-gray-900 py-4 px-4">
+      <div className="flex mt-4 space-x-6 sm:justify-center sm:mt-0 items-center">
+        <p className="text-md font-satoshi">
           Developed by {devName} &copy; {currentYear}
         </p>
-        <div class="flex space-x-6">
+        <div className="flex space-x-6">
           <Link href={devLinkedIn} target="_blank">
             <FaLinkedin size={20} color="blue" />
           </Link>
